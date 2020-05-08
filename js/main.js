aa62e0b61f75ec30ea652dc5745d2f09a10660c3
Dmitriy Skaliun <maildropbox048@gmail.com>
2020-05-08 21:12:20 +0300

    first part

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrow-next.svg" alt="arrow next"></img></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/arrow-prev.svg" alt="arrow prev"></img></button>',
+        fade: true,
+        dots: true
+
+    });
+})
