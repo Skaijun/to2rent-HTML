eb8190b9db27b32592c20fb193177f04b138339b
Dmitriy Skaliun <maildropbox048@gmail.com>
2020-05-10 14:56:18 +0300

    responsive webpage

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ $(function () {
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrow-next.svg" alt="arrow next"></img></button>',
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/arrow-prev.svg" alt="arrow prev"></img></button>',
         fade: true,
-        dots: true
-
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 451,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 })
